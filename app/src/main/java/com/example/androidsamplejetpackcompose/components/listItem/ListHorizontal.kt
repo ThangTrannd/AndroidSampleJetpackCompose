fd103ee2d854814fbd4ff966789fe5fd6a7b2f11
ThangTrannd <105451858+ThangTrannd@users.noreply.github.com>
2023-04-14 17:48:45 +0700

    temp
    temp list

--- app/src/main/java/com/example/androidsamplejetpackcompose/components/listItem/ListHorizontal.kt
+++ app/src/main/java/com/example/androidsamplejetpackcompose/components/listItem/ListHorizontal.kt
@@ -1,14 +1,43 @@
 package com.example.androidsamplejetpackcompose.components.listItem
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.androidsamplejetpackcompose.model.ItemDetail
 
 @Composable
-fun ListHorizontal(var list : List<T>){
+fun ListHorizontal(list : List<String>){
     LazyRow(){
-        items(items = list)
+        items(items = list, itemContent = {item ->
+            when(item){
+                "A" -> {
+
+                }
+                "B" -> {
+
+                }
+                "C" -> {
+
+                }
+                "D" -> {
+
+                }
+                else -> {
+
+                }
+            }
+        })
+    }
+}
+
+@Composable
+fun ItemList(data : ItemDetail){
+    Box {
+        Image(painter = painterResource(id = data.image), contentDescription = "image")
     }
 }
 
---
+++ app/src/main/java/com/example/androidsamplejetpackcompose/model/ItemDetail.kt
@@ -0,0 +1,7 @@
+package com.example.androidsamplejetpackcompose.model
+
+data class ItemDetail(
+    var title : String,
+    var image : Int,
+    var view : String
+)
